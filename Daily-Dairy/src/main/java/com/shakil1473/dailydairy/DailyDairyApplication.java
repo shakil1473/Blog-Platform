8aade212d1d857fd529605efcc4293f4916cf2d4
shakil mahmud <shakil1473@gmail.com>
2025-12-17 11:13:53 +0100

    Created the project
    - Created the project
    - Added the basic dependecies.

---
+++ Daily-Dairy/src/main/java/com/shakil1473/dailydairy/DailyDairyApplication.java
@@ -0,0 +1,13 @@
+package com.shakil1473.dailydairy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyDairyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyDairyApplication.class, args);
+    }
+
+}
---
+++ Daily-Dairy/src/test/java/com/shakil1473/dailydairy/DailyDairyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shakil1473.dailydairy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyDairyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
