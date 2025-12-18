06ab5a8994ce6e1a7774160d07fa74bc9eb764b9
shakil mahmud <shakil1473@gmail.com>
2025-12-18 14:44:57 +0100

    Temporary user and Fix path
    - Fixed the request path for auth controller to match frontend.
    - Added an user if not present for testing.

--- Daily-Dairy/src/main/java/com/shakil1473/dailydairy/config/SecurityConfig.java
+++ Daily-Dairy/src/main/java/com/shakil1473/dailydairy/config/SecurityConfig.java
@@ -1,5 +1,10 @@
 package com.shakil1473.dailydairy.config;
 
+import com.shakil1473.dailydairy.domain.entity.User;
+import com.shakil1473.dailydairy.repository.UserRepository;
+import com.shakil1473.dailydairy.security.BlogUserDetailsService;
+import com.shakil1473.dailydairy.security.JwtAuthenticationFilter;
+import com.shakil1473.dailydairy.service.AuthenticationService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,18 +13,44 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationConverter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public JwtAuthenticationFilter jwtAuthenticationFilter(AuthenticationService authenticationService) {
+        return new JwtAuthenticationFilter(authenticationService);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        BlogUserDetailsService blogUserDetailsService = new BlogUserDetailsService(userRepository);
+
+        String email = "shakil1473@gmail.com";
+        userRepository.findByEmail(email).orElseGet(() -> {
+            User newUser = User.builder()
+                    .fullName("Shakil Mahmud")
+                    .email(email)
+                    .password(passwordEncoder().encode("password1473"))
+                    .build();
+
+            return userRepository.save(newUser);
+        });
+
+        return blogUserDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+                                                   JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
         http
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/posts/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/categories/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/tags/**").permitAll()
@@ -28,7 +59,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                );
+                ).addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
--- Daily-Dairy/src/main/java/com/shakil1473/dailydairy/controller/AuthController.java
+++ Daily-Dairy/src/main/java/com/shakil1473/dailydairy/controller/AuthController.java
@@ -7,17 +7,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/api/v1/auth")
+@RequestMapping(path = "/api/v1/auth/login")
 @RequiredArgsConstructor
 public class AuthController {
 
     private final AuthenticationService authenticationService;
 
+    @PostMapping
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequestDto loginRequestDto) {
         UserDetails userDetails = authenticationService.authenticate(
                 loginRequestDto.getEmail(),
