eede5fb426dccc286bb939a1c39eaf1be1971bc9
shakil mahmud <shakil1473@gmail.com>
2025-12-18 13:11:55 +0100

    JWT authentication
    - added the authentication secret key in the application.properties file.
    - Added Auth controller class for handling authentication
    - Added other classes related to login authentication.

---
+++ Daily-Dairy/src/main/java/com/shakil1473/dailydairy/controller/AuthController.java
@@ -0,0 +1,36 @@
+package com.shakil1473.dailydairy.controller;
+
+import com.shakil1473.dailydairy.domain.dto.AuthResponse;
+import com.shakil1473.dailydairy.domain.dto.LoginRequestDto;
+import com.shakil1473.dailydairy.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationService authenticationService;
+
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequestDto loginRequestDto) {
+        UserDetails userDetails = authenticationService.authenticate(
+                loginRequestDto.getEmail(),
+                loginRequestDto.getPassword()
+        );
+
+        String tokenValue = authenticationService.generateToken(userDetails);
+        AuthResponse authResponse =  AuthResponse.builder()
+                .token(tokenValue)
+                .expiresIn(3600)
+                .build();
+
+        return ResponseEntity.ok(authResponse);
+    }
+
+}
---
+++ Daily-Dairy/src/main/java/com/shakil1473/dailydairy/service/implementations/AuthenticationServiceImplementation.java
@@ -0,0 +1,77 @@
+package com.shakil1473.dailydairy.service.implementations;
+
+import com.shakil1473.dailydairy.service.AuthenticationService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationServiceImplementation implements AuthenticationService {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsService userDetailsService;
+
+    @Value("${jwt.secret}")
+    private String jwtSecretKey;
+
+    private final Long jwtExpirationInMs = 360000000L;
+
+    @Override
+    public UserDetails authenticate(String email, String password) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(email, password)
+        );
+
+        return userDetailsService.loadUserByUsername(email);
+    }
+
+    @Override
+    public String generateToken(UserDetails userDetails) {
+
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + jwtExpirationInMs))
+                .signWith(getSigningKey())   // algorithm inferred
+                .compact();
+    }
+
+
+    @Override
+    public UserDetails validateToken(String token) {
+        String username = extractUsername(token);
+        return userDetailsService.loadUserByUsername(username);
+    }
+
+    private String extractUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+
+    // We need to return SecretKey from JJWT 0.13.0 dependency
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(
+                jwtSecretKey.getBytes(StandardCharsets.UTF_8)
+        );
+    }
+
+}
